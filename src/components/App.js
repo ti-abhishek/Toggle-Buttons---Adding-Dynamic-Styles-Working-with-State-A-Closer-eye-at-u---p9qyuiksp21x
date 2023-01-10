e0fe87c8c1779a008571634cb7420bf63ba9cb38
ti-abhishek <ti.abhishek15@gmail.com>
2023-01-10 21:36:28 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,54 @@
-import React, {useState} from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-//code here 
-  
+  //code here
+  const [buttonState, setButtonState] = useState({
+    id1: "OFF",
+    id2: "ON",
+  });
 
-  
+  const handleClick = (e) => {
+    // console.log(e.target.value)
+    // console.log(typeof e.target.innerText);
+    if (e.target.id === "btn1") {
+      if (e.target.innerText === "OFF") {
+        setButtonState({
+          id1: "ON",
+          id2: "OFF",
+        });
+      } else {
+        setButtonState({
+          id1: "OFF",
+          id2: "ON",
+        });
+      }
+    } else {
+      if (e.target.innerText === "ON") {
+        setButtonState({
+          id1: "ON",
+          id2: "OFF",
+        });
+      } else {
+        setButtonState({
+          id1: "OFF",
+          id2: "ON",
+        });
+      }
+    }
+  };
+
   return (
     <div className="App">
       <button id="btn1" onClick={handleClick}>
-      {/* assign value for button 1 */}
+        {/* assign value for button 1 */ buttonState.id1}
       </button>
       <br />
       <br />
       <button id="btn2" onClick={handleClick}>
-        {/* assign value for button 2 */}
+        {/* assign value for button 2 */ buttonState.id2}
       </button>
     </div>
   );
-}
-
+};
 
 export default App;
